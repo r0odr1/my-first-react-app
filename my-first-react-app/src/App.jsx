2f72e63af7aff3ace3a88356144ccb927ad240a8
Rodrigo <rhbon.08@gmail.com>
2023-06-26 19:43:29 -0500

    Correccion npm

--- my-first-react-app/src/App.jsx
+++ my-first-react-app/src/App.jsx
@@ -2,27 +2,27 @@ import './App.css'
 
 function App() {
   return (
-    <div>
-      <header>
-        <h1 class="title">404 NOT FOUND</h1>
-      </header>
-      <main class="container">
-        <div class="imagen">
-          <img src="./assets/scarecrow.png" alt=""></img>
-        </div>
-        
-        <article class="content-message">
-            <h1 class="font-title">I have bad news for you</h1>
-            <p class="font-paragrahp">
-                The page you are looking for might be removed 
-                or is temporarily unavailable.
-            </p>
-            <button class="button" type="button">BACK TO HOMEPAGE</button>
-        </article>
-      </main>
-    </div>
-      
-    
+    <>
+      <div>
+        <header>
+          <h1 class="title">404 NOT FOUND</h1>
+        </header>
+        <main class="container">
+          <div class="imagen">
+            <img src="./assets/scarecrow.png"/>
+          </div>
+          
+          <article class="content-message">
+              <h1 class="font-title">I have bad news for you</h1>
+              <p class="font-paragrahp">
+                  The page you are looking for might be removed 
+                  or is temporarily unavailable.
+              </p>
+              <button class="button" type="button">BACK TO HOMEPAGE</button>
+          </article>
+        </main>
+      </div>
+    </>
   )
 }
 
